bb5442876b1f5d659739d9b65136e3db7d615ca5
zodiitti <ernopi@gmail.com>
2015-03-18 21:34:34 +0200

    paska

---
+++ 3d avaruus/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float smooth = 2.0F;
+	public float tiltAngle = 30.0F;
+	public float pspeed = 5.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.position = new Vector3 (1, 1, 1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//updown
+		transform.Translate (Vector3.up * Input.GetAxis ("Vertical") * pspeed);
+
+		//rotate
+			float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle;
+			transform.rotation = Quaternion.Euler(270, tiltAroundY, 0);
+
+	}
+}
